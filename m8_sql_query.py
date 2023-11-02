d50972cf118e37a1031e71e0d63832d26ddff10d
js18user <93879664+js18user@users.noreply.github.com>
2023-11-02 23:59:32 +0200

    Add files via upload

---
+++ m8_sql_query.py
@@ -0,0 +1,14 @@
+sql_insert_query = ("INSERT "
+                    "INTO tickers "
+                    "(exchange, timestamp, symbol, big, ask) "
+                    "VALUES ($1,$2,$3,$4,$5)"
+                    )
+sql_create_table_query = ('CREATE TABLE IF NOT EXISTS tickers '
+                          ' (id SERIAL PRIMARY KEY, '
+                          '  exchange VARCHAR,   '
+                          '  timestamp BIGINT,   '
+                          '  symbol    VARCHAR,   ' 
+                          '  big       NUMERIC,   '
+                          '  ask       NUMERIC  );'
+                          )
+time_type = "asctime"
